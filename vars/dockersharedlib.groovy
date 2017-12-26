411283db0f6d04c04a47ca8a3421df9afd5800c9
pavaraj29 <pavan.kumarbnr@gmail.com>
2017-12-26 12:02:09 +0530

    Update dockersharedlib.groovy

--- vars/dockersharedlib.groovy
+++ vars/dockersharedlib.groovy
@@ -16,20 +16,6 @@ def call(body) {
                 stage ('Build') {
                     sh "echo 'building ${config.projectName} ...'"
                 }
-                stage ('Tests') {
-                    parallel 'static': {
-                        sh "echo 'shell scripts to run static tests...'"
-                    },
-                    'unit': {
-                        sh "echo 'shell scripts to run unit tests...'"
-                    },
-                    'integration': {
-                        sh "echo 'shell scripts to run integration tests...'"
-                    }
-                }
-                stage ('Deploy') {
-                    sh "echo 'deploying to server ${config.serverDomain}...'"
-                }
             } catch (err) {
                 currentBuild.result = 'FAILED'
                 throw err
