c57edf92a4e2604e8aa5520224e9d75aced19ed0
pavaraj29 <pavan.kumarbnr@gmail.com>
2017-12-26 17:08:16 +0530

    Update dockersharedlib.groovy

--- vars/dockersharedlib.groovy
+++ vars/dockersharedlib.groovy
@@ -17,7 +17,7 @@ def call(body) {
                 stage ('Docker image build') {
                     sh "ls"
                     sh "pwd"
-                    sh "cd node-js-sample &&  sudo docker build -t nodejs-image-new ."
+                    sh "sudo docker build -t nodejs-image-new ."
                 }
             } catch (err) {
                 currentBuild.result = 'FAILED'
