952b316ee4cdd5d6964d732cf5e9499d48524213
pavaraj29 <pavan.kumarbnr@gmail.com>
2017-12-27 23:12:42 +0530

    Update dockersharedlib.groovy

--- vars/dockersharedlib.groovy
+++ vars/dockersharedlib.groovy
@@ -18,11 +18,11 @@ def call(body) {
                     sh "sudo docker build -t ${config.dockerImageName} ."
                 }
                 stage ('Docker image tag') {
-                        sh "sudo  docker tag ${config.dockerImageName} ${config.image}"
+                        sh "sudo  docker tag ${config.dockerImageName} ${config.image}:${config.imageVersion}"
                 }  
                 stage ('Docker image push') {
                         sh "sudo docker login -u pavanraj29 -p Pavan@123"
-                        sh "sudo docker push ${config.image}"
+                        sh "sudo docker push ${config.image}:${config.imageVersion}"
                 }      
             } catch (err) {
                 currentBuild.result = 'FAILED'
