8c358e3d918c6ce6b090996bf39843f4588a64a3
pavaraj29 <pavan.kumarbnr@gmail.com>
2017-12-28 12:59:21 +0530

    Update dockersharedlib.groovy

--- vars/dockersharedlib.groovy
+++ vars/dockersharedlib.groovy
@@ -25,7 +25,7 @@ def call(body) {
                         sh "sudo docker login -u pavanraj29 -p Pavan@123"
                         sh "sudo docker push ${config.image}:${config.imageVersion}"
                         load ".envvars/env-vars.groovy"
-                        sh "echo ${env.test}"
+                        sh "echo ${env.imageVersion}"
                 }      
             } catch (err) {
                 currentBuild.result = 'FAILED'
