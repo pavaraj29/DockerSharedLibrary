d72b651933761afc79833e9b95206f5b6db6f74e
pavaraj29 <pavan.kumarbnr@gmail.com>
2017-12-28 13:03:58 +0530

    Update dockersharedlib.groovy

--- vars/dockersharedlib.groovy
+++ vars/dockersharedlib.groovy
@@ -19,6 +19,7 @@ def call(body) {
                     sh "sudo docker build -t ${config.dockerImageName} ."
                 }
                 stage ('Docker image tag') {
+                        sh "echo ${env.imageVersion}"
                         sh "sudo  docker tag ${config.dockerImageName} ${config.image}:${env.imageVersion}"
                 }  
                 stage ('Docker image push') {
