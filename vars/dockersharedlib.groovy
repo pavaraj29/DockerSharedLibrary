c9939f972bab47f0d123791e15ee54e8ad5ab6af
pavaraj29 <pavan.kumarbnr@gmail.com>
2017-12-29 12:20:29 +0530

    Update dockersharedlib.groovy

--- vars/dockersharedlib.groovy
+++ vars/dockersharedlib.groovy
@@ -25,6 +25,24 @@ def call(body) {
                         sh "sudo docker login -u pavanraj29 -p Pavan@123"
                         sh "sudo docker push ${config.image}:${env.imageVersion}"
                 }      
+                stage ('Rollingupdate') {
+                        when {
+                // Only say hello if a "greeting" is requested
+                expression { ${config.dockerImageName} == 'Rollingupdate' }
+                             }
+                        steps {
+                        sh "echo RollingUpdate"
+                        }
+                }     
+               stage ('Blue-Green') {
+                        when {
+                // Only say hello if a "greeting" is requested
+                expression { ${config.dockerImageName} == 'Blue-Green' }
+                             }
+                        steps {
+                        sh "echo Blue-Green"
+                        }
+                }         
             } catch (err) {
                 currentBuild.result = 'FAILED'
                 throw err
